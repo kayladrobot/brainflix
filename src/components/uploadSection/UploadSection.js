c11339a43f9cb71778c3290cfdc0d0c4a6664d9e
“Kayla <kayladrobot@gmail.com>
2023-02-20 21:20:35 -0800

    style tablet upload section

--- src/components/uploadSection/UploadSection.js
+++ src/components/uploadSection/UploadSection.js
@@ -5,32 +5,36 @@ import './UploadSection.scss'
 
 function UploadSection() {
   return (
-    <div className="upload__section">
+    <div className="upload">
         <h1 className="upload__header">Upload Video</h1>
-        <h3>Video thumbnail</h3>
-        <img 
-            src={testImg} 
-            alt="mohan murage"
-            className="upload__img"
-        />
+        <div className="upload__imgContainer">
+            <label>Video thumbnail</label>
+            <img 
+                src={testImg} 
+                alt="mohan murage"
+                className="upload__img"
+            />
+        </div>
         <form className="upload__form">
             <div className="upload__input">
                 <label>Title your Video</label>
-                <input name="comment" placeholder="Add a title to your video" type="text"></input>
+                <input className="upload__inputField inputField__title" name="comment" placeholder="Add a title to your video" type="text"></input>
             </div>
             <div className="upload__input">
                 <label>Add a Video Description</label>
-                <input name="comment" placeholder="Add a description to your video" type="text"></input>
+                <input className="upload__inputField inputField__description" name="comment" placeholder="Add a description to your video" type="text"></input>
             </div>
-            <button className="btn">
+            <div className="upload__cta">
+            <button className="btn upload__btn">
                 <img 
                     src={uploadIcon}
                     alt="upload icon"
                     className="upload__icon"/>
                 Publish
             </button>
+            <h2 className="upload__cancel">Cancel</h2>
+            </div>
         </form>
-        <h2>Cancel</h2>
     </div>
   )
 }
